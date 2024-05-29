81e1b72b9a58823bb9e3b94648a7a3c106ed7f1a
Billie Zhang <billie.zhang1504@gmail.com>
2024-05-29 18:05:25 -0400

    add error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
     loader: dashboardLoader,
+    errorElement: <Error />,
   },
 ]);
 
